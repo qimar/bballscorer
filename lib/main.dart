7e818c4fdedc7d4ef6dff32c8cddd7a676663b76
Ramiq Waqas <ramiqwaqas@gmail.com>
2023-11-21 15:45:10 -0800

    added multi providers userprovider, gamesprovider in material app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,39 @@
+import 'package:SportRabbit/providers/GamesProvider.dart';
+import 'package:SportRabbit/providers/UserProvider.dart';
 import 'package:SportRabbit/screens/games_list/games_listview.dart';
 import 'package:SportRabbit/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 // this entry point of flutter
 void main() {
-  runApp(const LoginApp());
+  runApp(const GamesRabbitApp());
 }
 
-class LoginApp extends StatelessWidget {
-  const LoginApp({super.key});
+class GamesRabbitApp extends StatelessWidget {
+  const GamesRabbitApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        restorationScopeId: 'com.GamesRabbit.app',
-        title: 'GamesRabbit',
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(
-                seedColor: const Color.fromARGB(255, 68, 9, 9)),
-            useMaterial3: true),
-        darkTheme: ThemeData.dark(),
-        routes: {
-          "/": (context) => const GamesListView(),
-          "/logn": (context) => const LoginScreen(),
-          //  "/": (context) => const HomePage(),
-          // "/search_page": (context) => const SearchPage(),
-          // "/player_page": (context) => const PlayerPage(),
-        });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+        ChangeNotifierProvider(create: (context) => GamesProvider()),
+      ],
+      child: MaterialApp(
+          restorationScopeId: 'com.GamesRabbit.app',
+          title: 'GamesRabbit',
+          theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: const Color.fromARGB(255, 68, 9, 9)),
+              useMaterial3: true),
+          darkTheme: ThemeData.dark(),
+          routes: {
+            "/": (context) => const GamesListView(),
+            "/logn": (context) => const LoginScreen(),
+            //  "/": (context) => const HomePage(),
+            // "/search_page": (context) => const SearchPage(),
+            // "/player_page": (context) => const PlayerPage(),
+          }),
+    );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:SportRabbit/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const LoginApp());
+    await tester.pumpWidget(const GamesRabbitApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
